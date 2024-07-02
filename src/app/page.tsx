edf9077d99b1a8a53c9907e843678aa85a786c8e
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-02 18:41:24 -0300

    correct content padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <main className="px-32 py-10">
       <Header />
     </main>
   );
