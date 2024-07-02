e1cbcbc7a44cd4b46c6b4367c413a0b10e12d9b4
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-02 18:54:28 -0300

    feat: add dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" suppressHydrationWarning>
       <link rel="shortcut icon" href="favicon.png" type="image/png" />
-      <body className={cn(inter.className, "bg-zinc-950 text-zinc-100")}>
-        {children}
-      </body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body className={cn(inter.className, "bg-zinc-950 text-zinc-100")}>
+          {children}
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
