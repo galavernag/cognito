9d86dce526f3f18020c63f220051921da6e1b9e0
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-03 03:24:54 -0300

    using middleware

--- apps/server/src/routes/auth-routes.ts
+++ apps/server/src/routes/auth-routes.ts
@@ -1,7 +1,7 @@
 import { AuthController } from "@/controllers/auth-controller";
 import { FastifyInstance } from "fastify";
 
-export default async function (fastify: FastifyInstance, options: any) {
+export default async function (fastify: FastifyInstance) {
   const authController = new AuthController();
 
   fastify.post("/auth/login", authController.login);
