ab5bbe808282e0dc6aaa90d921d83f292d0fb931
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-03 14:07:34 -0300

    create school creation

--- apps/server/src/controllers/school-controller.ts
+++ apps/server/src/controllers/school-controller.ts
@@ -5,20 +5,49 @@ import { z } from "zod";
 export class SchoolController {
   async getSchools(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const getSchoolsBodySchema = z.object({
-        id: z.string(),
-      });
-
-      const { id } = getSchoolsBodySchema.parse(request.params);
       const schools = await prisma.school.findMany({
         where: {
           members: {
-            some: { id },
+            some: { id: request.userId },
           },
         },
       });
 
       return reply.status(200).send({ schools });
+    } catch (error: any) {
+      return reply.status(500).send({ error: error.message });
+    }
+  }
+
+  async createSchool(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const createSchoolBodySchema = z.object({
+        name: z.string(),
+        domain: z.string(),
+      });
+
+      const { name, domain } = createSchoolBodySchema.parse(request.body);
+
+      const schoolAlreadyExistsWithDomain = await prisma.school.findFirst({
+        where: {
+          domain,
+        },
+      });
+
+      if (schoolAlreadyExistsWithDomain) {
+        return reply.status(400).send({
+          error: "This domain is already in use",
+        });
+      }
+
+      const school = await prisma.school.create({
+        data: {
+          name,
+          domain,
+        },
+      });
+
+      return reply.status(201).send({ school });
     } catch (error: any) {
       return reply.status(500).send({ error: error.message });
     }
--- apps/server/src/routes/school-routes.ts
+++ apps/server/src/routes/school-routes.ts
@@ -5,9 +5,10 @@ import { FastifyInstance } from "fastify";
 export default async function (fastify: FastifyInstance, options: object) {
   const schoolController = new SchoolController();
 
-  fastify.get(
-    "/schools/:id",
+  fastify.get("/schools", { preHandler: auth }, schoolController.getSchools);
+  fastify.post(
+    "/school/create",
     { preHandler: auth },
-    schoolController.getSchools
+    schoolController.createSchool
   );
 }
