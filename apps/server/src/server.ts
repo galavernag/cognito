a9d984d940c8187a7cefb843bbd2c92dcaf826b7
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-03 15:44:09 -0300

    load test routes

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 import authRoutes from "@/routes/auth-routes";
 import schoolRoutes from "./routes/school-routes";
 import inviteRoutes from "./routes/invite-routes";
+import testRoutes from "./routes/test-routes";
 
 const port = 3333;
 const server = Fastify({
@@ -12,6 +13,7 @@ const server = Fastify({
 server.register(authRoutes);
 server.register(schoolRoutes);
 server.register(inviteRoutes);
+server.register(testRoutes);
 
 server.listen({ port }, (err, address) => {
   if (err) {
