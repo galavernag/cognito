ee8fb403b7816344b8da5cdcc7dfa3401ec75550
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-03 18:36:40 -0300

    export auth, invite, test and school service

--- packages/services/index.ts
+++ packages/services/index.ts
@@ -1,3 +1,7 @@
-export * from "./services/auth";
 export * from "./services/bcrypt";
 export * from "./services/jwt";
+
+export * from "./services/auth";
+export * from "./services/invite";
+export * from "./services/test";
+export * from "./services/school";
