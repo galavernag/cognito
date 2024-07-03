6f218bac165013f4656ddc19aa15dfabaae3c433
Guilherme Galaverna <guigalaverna@gmail.com>
2024-07-03 02:57:37 -0300

    implement login and registration

--- apps/server/src/controllers/auth-controller.ts
+++ apps/server/src/controllers/auth-controller.ts
@@ -1,15 +1,84 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
+import { z } from "zod";
+import { prisma } from "@/services/prisma";
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const jwtSecret = process.env.JWT_SECRET!;
+const bcryptSalt = Number(process.env.BCRYPT_SALT);
+
 export class AuthController {
   async login(request: FastifyRequest, reply: FastifyReply) {
-    return {
-      message: "Hello World",
-    };
+    const loginBodySchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = loginBodySchema.parse(request.body);
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return reply.status(400).send({
+        message: "User not found",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return reply.status(400).send({
+        message: "Password is incorrect",
+      });
+    }
+
+    const token = jwt.sign({ userId: user.id }, jwtSecret, {
+      expiresIn: "7d",
+    });
+
+    return reply.status(200).send({
+      token,
+    });
   }
 
   async register(request: FastifyRequest, reply: FastifyReply) {
-    return {
-      message: "Hello World",
-    };
+    const registerBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { name, email, password } = registerBodySchema.parse(request.body);
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return reply.status(400).send({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    const token = jwt.sign({ userId: newUser.id }, jwtSecret, {
+      expiresIn: "7d",
+    });
+    return reply.status(201).send({
+      token,
+    });
   }
 }
