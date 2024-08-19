bd007f89907333feca9082c2a2521105235951d0
Guilherme Galaverna <guigalaverna@gmail.com>
2024-08-19 15:33:39 -0300

    update env variables

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -1,8 +1,13 @@
 import z from "zod";
 
 const schema = z.object({
-  JWT_SECRET: z.coerce.string(),
+  JWT_SECRET: z.preprocess((val) => {
+    const secret = new TextEncoder().encode(String(val));
+
+    return secret;
+  }, z.instanceof(Uint8Array)),
   SALT_ROUNDS: z.coerce.number(),
 });
 
 export const env = schema.parse(process.env);
+Uint8Array;
