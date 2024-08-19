8dda9928906f026f9728c86fc8a819310a80ae33
Guilherme Galaverna <guigalaverna@gmail.com>
2024-08-19 15:58:08 -0300

    protect app route

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,19 +4,20 @@ import { redirect } from "next/navigation";
 import { env } from "./lib/env";
 import type { NextRequest } from "next/server";
 import * as jose from "jose";
+import { get_user } from "./actions/get-user";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const token = cookies().get("cognito.token");
+  const cookiesStore = cookies();
+  const token = cookiesStore.get("cognito.token");
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  const verified = await jose.jwtVerify(token.value, env.JWT_SECRET);
-  console.log(verified);
+  const { payload } = await jose.jwtVerify(token.value, env.JWT_SECRET);
 
-  if (!verified) {
+  if (!payload) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
