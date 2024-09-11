e843dc23d4a2433cf842426d854ac77c85a20397
Guilherme Galaverna <guigalaverna@gmail.com>
2024-09-11 18:06:39 -0300

    feat: adicionando uma screenshot da plataforma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import CognitoLogo from "@/assets/logo.svg";
+import CognitoScreenshot from "@/assets/cognito-screenshot.png";
+
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
@@ -7,13 +10,17 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Syne } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
+const syne = Syne({ subsets: ["latin"] });
+
 export default function Landing() {
   return (
-    <main className="max-w-[1680px] mx-auto mt-10">
-      <header className="flex items-center justify-between">
+    <main className="max-w-[1680px] mx-auto mt-10 flex flex-col items-center">
+      <header className="flex items-center justify-between w-full">
         <div className="flex items-center gap-3">
           <Image src={CognitoLogo} className="size-8" alt="Logo do Cognito" />
           <span className="text-xl font-bold text-zinc-50">Cognito</span>
@@ -50,6 +57,32 @@ export default function Landing() {
           </Link>
         </div>
       </header>
+
+      <section className="mt-32 text-center space-y-10">
+        <Badge className="w-auto py-1.5" variant="outline">
+          Lançamento da versão v0.1
+        </Badge>
+        <h1
+          className={cn(
+            syne.className,
+            "text-5xl text-center font-semibold leading-tight"
+          )}
+        >
+          Eleve a experiência da gestão <br />
+          de avaliações na sua escola.
+        </h1>
+        <p className="text-lg text-zinc-700">
+          Facilite a gestão de avalições na sua escola com uma aplicação que faz{" "}
+          <br />
+          todo o processo difícil para você.
+        </p>
+
+        <Image
+          className="border rounded-lg w-[1000px]"
+          src={CognitoScreenshot}
+          alt="Captura de tela do painel do Cognito"
+        />
+      </section>
     </main>
   );
 }
